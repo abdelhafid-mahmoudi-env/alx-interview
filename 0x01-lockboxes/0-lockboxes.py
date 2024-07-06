cc624f6d13c1f1ccd17641d200ecbb7a78b4d5cd
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-06 11:08:15 +0000

    test

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,14 +1,30 @@
+#!/usr/bin/python3
+"""
+Solution to the lockboxes problem.
+"""
+
 def canUnlockAll(boxes):
-    n = len(boxes)
-    visited = set()
-    stack = [0]
+    """
+    Determines whether a series of locked boxes can be opened
+    based on keys that can be attained.
 
-    while stack:
-        box = stack.pop()
-        if box not in visited:
-            visited.add(box)
-            for key in boxes[box]:
-                if key < n:
-                    stack.append(key)
+    Args:
+        boxes (list): A list of lists where each sublist represents a box and contains keys to other boxes.
 
-    return len(visited) == n
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    if type(boxes) is not list:
+        return False
+    elif len(boxes) == 0:
+        return False
+
+    for key in range(1, len(boxes) - 1):
+        is_box_checked = False
+        for box_index in range(len(boxes)):
+            is_box_checked = key in boxes[box_index] and key != box_index
+            if is_box_checked:
+                break
+        if is_box_checked is False:
+            return is_box_checked
+    return True
