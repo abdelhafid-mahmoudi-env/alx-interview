d3472526fe792d59bd3e0ed7331db6c9e282a440
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-09 19:09:52 +0000

    measure

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,31 +1,15 @@
 #!/usr/bin/python3
 """
-Module for minimum operations to achieve exactly n 'H' characters.
+Function that calculates the min operations to copy and paste letters
 """
 
 
-def min_operations(n):
-    """
-    min_operations
-    Calculate the minimum number of operations.
-
-    Parameters:
-    n (int): The desired number of 'H' characters.
-
-    Returns:
-    int: The minimum number of operations.
-    """
-    if n < 2:
-        return 0
-
-    operations = 0
-    divisor = 2
-
-    while divisor <= n:
-        if n % divisor == 0:
-            operations += divisor
-            n //= divisor
-            divisor -= 1
-        divisor += 1
-
-    return operations
+def minOperations(n):
+    nOpe = 0
+    minOpe = 2
+    while n > 1:
+        while n % minOpe == 0:
+            nOpe += minOpe
+            n /= minOpe
+        minOpe += 1
+    return nOpe
