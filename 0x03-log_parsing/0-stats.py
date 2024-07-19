1b9d7d8a1117bd6867a095810bb77e8198a30c89
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-07-19 02:35:03 +0000

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,37 @@
 #!/usr/bin/python3
+"""
+Log parsing
+"""
+
 import sys
-import signal
-import re
 
-total_file_size = 0
-status_codes_count = {}
-valid_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+filesize, count = 0, 0
+codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+stats = {k: 0 for k in codes}
 
-def print_stats():
-    print("File size: {}".format(total_file_size))
-    for code in sorted(status_codes_count.keys()):
-        if status_codes_count[code] > 0:
-            print("{}: {}".format(code, status_codes_count[code]))
-
-def signal_handler(sig, frame):
-    print_stats()
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
-
-line_count = 0
+def echo_stats(stats: dict, file_size: int) -> None:
+    print("File size: {:d}".format(filesize))
+    for k, v in sorted(stats.items()):
+        if v:
+            print("{}: {}".format(k, v))
 
 try:
     for line in sys.stdin:
-        match = re.match(r"^(\S+) - \[.*\] \"GET /projects/260 HTTP/1.1\" (\d+) (\d+)$", line)
-        if match:
-            status_code = int(match.group(2))
-            file_size = int(match.group(3))
-            
-            total_file_size += file_size
-            
-            if status_code in valid_status_codes:
-                if status_code not in status_codes_count:
-                    status_codes_count[status_code] = 0
-                status_codes_count[status_code] += 1
-
-            line_count += 1
-
-            if line_count % 10 == 0:
-                print_stats()
-
-except Exception as e:
-    pass
-finally:
-    print_stats()
+        count += 1
+        data = line.split()
+        try:
+            status_code = data[-2]
+            if status_code in stats:
+                stats[status_code] += 1
+        except BaseException:
+            pass
+        try:
+            filesize += int(data[-1])
+        except BaseException:
+            pass
+        if count % 10 == 0:
+            echo_stats(stats, filesize)
+    echo_stats(stats, filesize)
+except KeyboardInterrupt:
+    echo_stats(stats, filesize)
+    raise
